8941a533474b6778b563e8d6dd7b41295b3585e8
coder1992 <462326676@qq.com>
2026-04-06 23:51:57 +0800

    Add files via upload

---
+++ trans.py
@@ -0,0 +1,17 @@
+str1='0b101010'
+print(int(str1,2))
+
+
+a=0
+b=5
+c=None
+print(a and b)
+print(a or b)
+print(a and c)
+print(c and a)
+d=10
+print(b and d)
+
+x1=255
+x2=128
+print(bin(x1 % x2))
